d0cb5e09e87ac0775bca9a94b45728244e259cd7
Rachel Fish <itsrachelfish@gmail.com>
2013-04-20 17:54:32 +0400

    Mission accomplished

--- count.js
+++ count.js
@@ -1,18 +1,44 @@
-var app = require('express')()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
+var express = require('express')
+  , app     = require('express')()
+  , server  = require('http').createServer(app)
+  , io      = require('socket.io').listen(server)
+  , count   = 0;
+
+function getDate()
+{
+    var date = new Date();
+    
+    var   day   = date.getDate()
+        , month = date.getMonth() + 1
+        , year  = date.getFullYear()
+        , hour  = date.getHours()
+        , min   = date.getMinutes()
+        , sec   = date.getSeconds();
+
+    return day+'-'+month+'-'+year+' '+hour+':'+min+':'+sec;
+}
 
 server.listen(1024);
 
-app.get('/', function (req, res) {
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', function (req, res)
+{
   res.sendfile(__dirname + '/index.html');
 });
 
-io.sockets.on('connection', function (socket) {
-  io.sockets.emit('connected', { 'connection': 'established' });
+io.sockets.on('connection', function (socket)
+{
+    count++;
+    io.sockets.emit('count', {users: count});
 
-  socket.on('name', function (data) {
-    console.log(data);
-    io.sockets.emit('name', data);
-  });
+    console.log(getDate() + ' User connected');
+
+    socket.on('disconnect', function ()
+    {
+        console.log(getDate() + ' User disconnected');
+        count--;
+        
+        io.sockets.emit('count', {users: count});
+    });
 });
